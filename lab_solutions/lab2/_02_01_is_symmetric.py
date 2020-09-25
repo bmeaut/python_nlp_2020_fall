507f0fd594a0ab9e7cf4f4043a831524df1259f8
Attila Nagy <attila.nagy@meltwater.com>
2020-09-25 17:56:45 +0200

    add optimized solution to lab2.01

--- lab_solutions/lab2/_02_01_is_symmetric.py
+++ lab_solutions/lab2/_02_01_is_symmetric.py
@@ -10,12 +10,34 @@ def is_symmetric(l):
     return list(reversed(l)) == list(l)
 
 
+def is_symmetric_opt(l):
+    length = len(l)
+    first = 0
+    last = length - 1
+    is_sym = True
+    while first < last:
+        if l[first] == l[last]:
+            first = first + 1
+            last = last - 1
+        else:
+            is_sym = False
+            break
+
+    return is_sym
+
+
 def main():
     assert is_symmetric([1]) == True
     assert is_symmetric([]) == True
     assert is_symmetric([1, 2, 3, 1]) == False
     assert is_symmetric([1, "foo", "bar", "foo", 1]) == True
     assert is_symmetric("abcba") == True
+
+    assert is_symmetric_opt([1]) == True
+    assert is_symmetric_opt([]) == True
+    assert is_symmetric_opt([1, 2, 3, 1]) == False
+    assert is_symmetric_opt([1, "foo", "bar", "foo", 1]) == True
+    assert is_symmetric_opt("abcba") == True
 
     print("Tests passed.")
 
