7f0e5caa02c273a7f85e0d876d2a1dcfb376867f
Judit Acs <judit@sch.bme.hu>
2020-11-18 11:47:17 +0100

    Lecture 9

---
+++ lectures/img/tikz/rename_image.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#
+# rename_image.sh
+# Copyright (C) 2020 Judit Acs <judit@sch.bme.hu>
+#
+# Distributed under terms of the MIT license.
+#
+
+mv sequence_modeling_figures-0.png abstract_sequence_classification.png
+mv sequence_modeling_figures-1.png abstract_sequence_tagging.png
+mv sequence_modeling_figures-2.png abstract_seq2seq.png
+mv sequence_modeling_figures-3.png example_sequence_classification.png
+mv sequence_modeling_figures-4.png example_sequence_tagging.png
+mv sequence_modeling_figures-5.png example_seq2seq.png
+mv sequence_modeling_figures-6.png padding.png
+mv sequence_modeling_figures-7.png padding_last_highlight.png
+mv sequence_modeling_figures-8.png hungarian_case_lstm.png
